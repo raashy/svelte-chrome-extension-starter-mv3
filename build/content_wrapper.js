6bd463225b4711c8474ce9d1c4e608f8a4484ef5
Rahit Ash <hi@rashy.me>
2024-02-28 10:35:46 +0530

    Modified to fit as a npm package

--- build/content_wrapper.js
+++
@@ -1,6 +0,0 @@
-// The content wrapper is a workaround as you cannot import es6 modules in a content script.
-
-(async () => {
-  const src = chrome.runtime.getURL("./content.js");
-  const contentScript = await import(src);
-})();
--- build/newtab.js
+++
@@ -1,1 +0,0 @@
-import{S as o,i as r,s as l,e as c,b as i,a as m,n as a,d as p}from"./assets/index-1e55a147.js";/* empty css                   */function d(n){let t;return{c(){t=c("h1"),t.textContent="Hello World!",i(t,"class","text-black")},m(e,s){m(e,t,s)},p:a,i:a,o:a,d(e){e&&p(t)}}}class u extends o{constructor(t){super(),r(this,t,null,d,l,{})}}new u({target:document.getElementById("app")});
